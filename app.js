d7176a5bbbabbbcded5a9254c56f711cf47ec5c1
AvazbekOlimov <olimovavazbeka@gmail.com>
2024-02-29 15:29:59 +0500

    functionality of add-item added

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const rangeInput = document.getElementById("rangeInput");
 const rangeLabel = document.getElementById("rangeLabel");
+const addButton = document.querySelector(".btn1");
+const subtractButton = document.querySelector(".btn2");
+let countElement = document.querySelector(".count-of-product");
 
-// Update label text when range input value changes
-rangeInput.addEventListener("input", function () {
-  let value = this.value;
-  // Insert a space after every third digit
-  value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  rangeLabel.textContent = value;
+// rangeInput.addEventListener("input", function () {
+//   let value = this.value;
+//   value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+//   rangeLabel.textContent = value;
+// });
+
+addButton.addEventListener("click", function () {
+  var currentCount = parseInt(countElement.innerText);
+  let newCount = currentCount + 1;
+  countElement.innerText = newCount;
+});
+
+subtractButton.addEventListener("click", function () {
+  var currentCount = parseInt(countElement.innerText);
+  if (currentCount != 1) {
+    let newCount = currentCount - 1;
+
+    countElement.innerText = newCount;
+  }
 });
