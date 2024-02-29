ec8d3696bd9746e15513f57a32482550b8d6e38b
AvazbekOlimov <olimovavazbeka@gmail.com>
2024-02-29 22:19:02 +0500

    product html and style end

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const rangeLabel = document.getElementById("rangeLabel");
 const addButton = document.querySelector(".btn1");
 const subtractButton = document.querySelector(".btn2");
 let countElement = document.querySelector(".count-of-product");
-
+const charasteristics = document.querySelector(".charasteristics");
+const description = document.querySelector(".description");
 // rangeInput.addEventListener("input", function () {
 //   let value = this.value;
 //   value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
@@ -23,4 +24,20 @@ subtractButton.addEventListener("click", function () {
 
     countElement.innerText = newCount;
   }
+});
+charasteristics.addEventListener("click", function () {
+  charasteristics.classList.add("active-btn");
+  description.classList.remove("active-btn");
+  const infoCharasteristics = document.querySelector(".characteristics-info");
+  const infoDescription = document.querySelector(".description-info");
+  infoDescription.classList.add("hidden");
+  infoCharasteristics.classList.remove("hidden");
+});
+description.addEventListener("click", function () {
+  charasteristics.classList.remove("active-btn");
+  description.classList.add("active-btn");
+  const infoCharasteristics = document.querySelector(".characteristics-info");
+  const infoDescription = document.querySelector(".description-info");
+  infoCharasteristics.classList.add("hidden");
+  infoDescription.classList.remove("hidden");
 });
